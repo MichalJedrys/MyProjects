88ccdd193700b2a02a652471d43dbf48d2e6fbcb
MichalJedrys <michal.jedrys1@gmail.com>
2024-07-17 16:24:00 +0200

    Delete old, unnecessary files

--- Other Tasks/Memory/Tests/ArrayTasks/Main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Copy-To-Two_Arrays.h"
-
-using namespace std;
-
-int main()
-{	
-	cout << "Project has been compiled and run." << endl;
-}
--- Other Tasks/STL/STL-Tasks/string_provider.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-class string_provider
-{
-public:
-	virtual std::string get_lines() = 0;
-	virtual ~string_provider() = default;
-};
